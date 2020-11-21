c0fad00d6482662ab3bba90c4da1648cda462f6b
shumiya-git <ShumiyaAwsTest@gmail.com>
2020-11-21 10:43:53 +0900

    レビュー一覧　検索モーダル完成

--- components/RestaurantList.js
+++ components/RestaurantList.js
@@ -10,20 +10,52 @@ import Pagination from "material-ui-flat-pagination";
 class RestaurantList extends Component{
     constructor(props){
         super(props);
+        let db = firebase.database();
+        let ref_CategoryList = db.ref('CategoryMst/');
+        let categoryList = [];
+        // カテゴリマスタ取得
+        ref_CategoryList.orderByKey().on('value', (snapshot)=>{
+            for(let i in snapshot.val()){
+                let categoryMst = snapshot.val()[i];
+                categoryList.push(categoryMst);
+            }
+        })
         this.state = {
             loading: true,
             review: [],
             detailModalShow: false,
             searchModalShow: false,
+            searchVisitDateFrom: '',
+            searchVisitDateTo: '',
+            searchName: '',
+            searchPriceFrom: '',
+            searchPriceTo: '',
+            searchScoreFrom: '',
+            searchScoreTo: '',
+            searchCategory: '',
+            searchStation: '',
             no: 0,
             offset: 0,
-            parPage: 10
+            parPage: 10,
+            categoryList: categoryList
         }
         this.detailModalShow = this.detailModalShow.bind(this);
         this.detailModalClose  = this.detailModalClose.bind(this);
         this.searchModalShow = this.searchModalShow.bind(this);
         this.searchModalClose  = this.searchModalClose.bind(this);
+        this.onChangeSearchVisitDateFrom  = this.onChangeSearchVisitDateFrom.bind(this);
+        this.onChangeSearchVisitDateTo  = this.onChangeSearchVisitDateTo.bind(this);
+        this.onChangeSearchName  = this.onChangeSearchName.bind(this);
+        this.onChangeSearchPriceFrom  = this.onChangeSearchPriceFrom.bind(this);
+        this.onChangeSearchPriceTo  = this.onChangeSearchPriceTo.bind(this);
+        this.onChangeSearchScoreFrom  = this.onChangeSearchScoreFrom.bind(this);
+        this.onChangeSearchScoreTo  = this.onChangeSearchScoreTo.bind(this);
+        this.onChangeSearchCategory  = this.onChangeSearchCategory.bind(this);
+        this.onChangeSearchStation  = this.onChangeSearchStation.bind(this);
+        this.doSearch  = this.doSearch.bind(this);
+        this.doClear  = this.doClear.bind(this);
         this.doDelete  = this.doDelete.bind(this);
+        this.createCategoryList = this.createCategoryList.bind(this);
     }
     
     // 詳細モーダル表示
@@ -57,6 +89,42 @@ class RestaurantList extends Component{
         this.setState({
             searchModalShow: false
          });
+    }
+
+    onChangeSearchVisitDateFrom(e){
+        this.setState({searchVisitDateFrom: e.target.value});
+    }
+
+    onChangeSearchVisitDateTo(e){
+        this.setState({searchVisitDateTo: e.target.value});
+    }
+
+    onChangeSearchName(e){
+        this.setState({searchName: e.target.value});
+    }
+
+    onChangeSearchPriceFrom(e){
+        this.setState({searchPriceFrom: e.target.value});
+    }
+
+    onChangeSearchPriceTo(e){
+        this.setState({searchPriceTo: e.target.value});
+    }
+
+    onChangeSearchScoreFrom(e){
+        this.setState({searchScoreFrom: e.target.value});
+    }
+
+    onChangeSearchScoreTo(e){
+        this.setState({searchScoreTo: e.target.value});
+    }
+
+    onChangeSearchCategory(e){
+        this.setState({searchCategory: e.target.value});
+    }
+
+    onChangeSearchStation(e){
+        this.setState({searchStation: e.target.value});
     }
 
     // ページネーション
@@ -121,12 +189,31 @@ class RestaurantList extends Component{
                 }
                 review.sort(function(val1,val2){
                     return ( val1.visitDate < val2.visitDate ? 1 : -1);
-                });    
+                });
             }
             this.setState({
                 review: review
             });
         });
+    }
+
+    //　カテゴリリスト作成
+    createCategoryList(){
+        let contentCategoryList = [];
+        contentCategoryList.push(
+            // 初期表示用のダミー
+            <option key={"category"} value=''></option>
+        )
+        for (let i in this.state.categoryList){
+            contentCategoryList.push(
+                <option key={"category" + i} value={this.state.categoryList[i]['name']}>{this.state.categoryList[i]['name']}</option>
+            )
+        }
+        return (
+            <Form.Control as="select" onChange={this.onChangeSearchCategory} value={this.state.searchCategory} >
+                {contentCategoryList}
+            </Form.Control>
+        );
     }
 
     // ヘッダーボタン作成
@@ -238,10 +325,12 @@ class RestaurantList extends Component{
                             <Col sm={3} key={'detailModalBodyHeaderStation'} className={common.tableHeader}>最寄り駅</Col>
                             <Col sm={9} key={'detailModalBodyStation'} className={common.tableBody}>{review[this.state.no]['station']}駅</Col>
                             <Col sm={3} key={'detailModalBodyHeaderReview'} className={common.tableHeader}>レビュー</Col>
-                            <Col sm={9} key={'detailModalBodyReview'} className={common.tableBody}>{review[this.state.no]['review']}</Col>
+                            <Col sm={9} key={'detailModalBodyReview'} className={common.tableBody}>
+                                {review[this.state.no]['review'] == '' ? '-' : review[this.state.no]['review']}
+                            </Col>
                             <Col sm={3} key={'detailModalBodyHeaderPhoto'} className={common.tableHeader}>画像</Col>
                             <Col sm={9} key={'detailModalBodyPhoto'} className={common.tableBody}>
-                                {review[this.state.no]['photo'] == '' ? '無し' : <Image src={this.state.photoURL}/>}
+                                {review[this.state.no]['photo'] == '' ? '-' : <Image src={this.state.photoURL}/>}
                             </Col>
                         </Row>
                     </Modal.Body>
@@ -275,50 +364,56 @@ class RestaurantList extends Component{
                                 <strong>来店日</strong>
                             </Col>
                             <Col xs={8} className={common.form_div}>
-                                <Form.Control type="text" className={common.date_text + ' ' + common.modal_text} placeholder="YYYY/MM/DD" maxLength='10' />
+                                <Form.Control type="text" className={common.date_text + ' ' + common.modal_date} placeholder="YYYY/MM/DD" maxLength='10' value={this.state.searchVisitDateFrom} onChange={this.onChangeSearchVisitDateFrom} />
+                                ～<Form.Control type="text" className={common.date_text + ' ' + common.modal_date} placeholder="YYYY/MM/DD" maxLength='10' value={this.state.searchVisitDateTo} onChange={this.onChangeSearchVisitDateTo} />
                             </Col>
                             <hr />
                             <Col xs={4} className={common.form_div + ' ' + common.modal_label}>
                                 <strong>レストラン名</strong>
                             </Col>
                             <Col xs={8} className={common.form_div}>
-                                <Form.Control type="text" className={common.date_text + ' ' + common.modal_text} />
+                                <Form.Control type="text" className={common.date_text + ' ' + common.modal_text} value={this.state.searchName} onChange={this.onChangeSearchName} />
                             </Col>
                             <hr />
                             <Col xs={4} className={common.form_div + ' ' + common.modal_label}>
                                 <strong>金額</strong>
                             </Col>
                             <Col xs={8} className={common.form_div}>
-                                <Form.Control type="number" className={common.date_text + ' ' + common.modal_text} min="0" />円
+                                <Form.Control type="number" className={common.date_text + ' ' + common.modal_number} min="0" value={this.state.searchPriceFrom} onChange={this.onChangeSearchPriceFrom} />～
+                                <Form.Control type="number" className={common.date_text + ' ' + common.modal_number} min="0" value={this.state.searchPriceTo} onChange={this.onChangeSearchPriceTo} />円
                             </Col>
                             <hr />
                             <Col xs={4} className={common.form_div + ' ' + common.modal_label}>
                                 <strong>点数</strong>
                             </Col>
                             <Col xs={8} className={common.form_div}>
-                                <Form.Control type="number" className={common.date_text + ' ' + common.modal_text} min="1" max="5" />点
+                                <Form.Control type="number" className={common.date_text + ' ' + common.modal_number} min="1" max="5" value={this.state.searchScoreFrom} onChange={this.onChangeSearchScoreFrom} />～
+                                <Form.Control type="number" className={common.date_text + ' ' + common.modal_number} min="1" max="5" value={this.state.searchScoreTo} onChange={this.onChangeSearchScoreTo} />点
                             </Col>
                             <hr />
                             <Col xs={4} className={common.form_div + ' ' + common.modal_label}>
                                 <strong>カテゴリ</strong>
                             </Col>
                             <Col xs={8} className={common.form_div}>
-                                <Form.Control type="text" className={common.date_text + ' ' + common.modal_text} />
+                                {this.createCategoryList()}
                             </Col>
                             <hr />
                             <Col xs={4} className={common.form_div + ' ' + common.modal_label}>
                                 <strong>最寄り駅</strong>
                             </Col>
                             <Col xs={8} className={common.form_div}>
-                                <Form.Control type="text" className={common.date_text + ' ' + common.modal_text} />駅
+                                <Form.Control type="text" className={common.date_text + ' ' + common.modal_text} value={this.state.searchStation} onChange={this.onChangeSearchStation} />駅
                             </Col>
                             <hr />
                         </Row>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="outline-danger" onClick={this.searchModalClose}>
+                    <Button variant="outline-danger" onClick={this.doSearch}>
                         検索
+                    </Button>
+                    <Button variant="outline-secondary" onClick={this.doClear}>
+                        クリア
                     </Button>
                     <Button variant="dark" onClick={this.searchModalClose}>
                         戻る
@@ -332,6 +427,110 @@ class RestaurantList extends Component{
                 {content}
             </div>
         )
+    }
+    // 表示データ取得
+    doSearch(){
+        let name = this.props.username;
+        if (name == ''){
+            return;
+        } else {
+            this.setState({loading:false});
+        }
+        let db = firebase.database();
+        let ref = db.ref('Reviews/');    
+        // キーを元にレビューを取得
+        ref.orderByKey().equalTo(name).on('value', (snapshot)=>{
+            let review = [];
+            if (snapshot.val() != null) {
+                let d = snapshot.val()[name];
+                for(let i in d){
+                    review.push(d[i]);
+                }
+                review.sort(function(val1,val2){
+                    return ( val1.visitDate < val2.visitDate ? 1 : -1);
+                });
+            }
+            review = this.doFilter(review);
+            this.setState({
+                review: review,
+                searchModalShow: false
+            });
+        });
+    }
+    // 検索_絞り込み
+    doFilter(review){
+        console.log('doFilter');
+
+        if (this.state.searchVisitDateFrom != '') {
+            let searchVisitDateFrom = this.state.searchVisitDateFrom;
+            review = review.filter(function(item, index){
+                if (item.visitDate >= searchVisitDateFrom) return true;
+            });
+        }
+        if (this.state.searchVisitDateTo != '') {
+            let searchVisitDateTo = this.state.searchVisitDateTo;
+            review = review.filter(function(item, index){
+                if (item.visitDate <= searchVisitDateTo) return true;
+            });
+        }
+        if (this.state.searchName != '') {
+            let searchName = this.state.searchName;
+            review = review.filter(function(item, index){
+                if (item.name.includes(searchName)) return true;
+            });
+        }
+        if (this.state.searchPriceFrom != '') {
+            let searchPriceFrom = this.state.searchPriceFrom;
+            review = review.filter(function(item, index){
+                if (item.price >= searchPriceFrom) return true;
+            });
+        }
+        if (this.state.searchPriceTo != '') {
+            let searchPriceTo = this.state.searchPriceTo;
+            review = review.filter(function(item, index){
+                if (item.price <= searchPriceTo) return true;
+            });
+        }
+        if (this.state.searchScoreFrom != '') {
+            let searchScoreFrom = this.state.searchScoreFrom;
+            review = review.filter(function(item, index){
+                if (item.score >= searchScoreFrom) return true;
+            });
+        }
+        if (this.state.searchScoreTo != '') {
+            let searchScoreTo = this.state.searchScoreTo;
+            review = review.filter(function(item, index){
+                if (item.score <= searchScoreTo) return true;
+            });
+        }
+        if (this.state.searchCategory != '') {
+            let searchCategory = this.state.searchCategory;
+            review = review.filter(function(item, index){
+                if (item.category == searchCategory) return true;
+            });
+        }
+        if (this.state.searchStation != '') {
+            let searchStation = this.state.searchStation;
+            review = review.filter(function(item, index){
+                if (item.station.includes(searchStation)) return true;
+            });
+        }
+        return review;
+    }
+
+    // 検索情報クリア
+    doClear(){
+        this.setState({
+            searchVisitDateFrom: '',
+            searchVisitDateTo: '',
+            searchName: '',
+            searchPriceFrom: '',
+            searchPriceTo: '',
+            searchScoreFrom: '',
+            searchScoreTo: '',
+            searchCategory: '',
+            searchStation: ''
+        });
     }
 
     // 削除
